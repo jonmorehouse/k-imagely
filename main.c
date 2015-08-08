7e7219aede4925a144b5a999599ae71b4d156868
Jon Morehouse <morehousej09@gmail.com>
2015-08-08 15:10:12 -0700

    temp: some more c-programming examples

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+void dialogue(tcpsock as) {
+  tcpsend(as, "What's your name?\r\n", 19, -1);
+
+  tcpclose(as);
+}
+
 int main(int argc, char *argv[]) {
   int port = 5555;
   if (argc > 1) {
@@ -20,9 +26,21 @@ int main(int argc, char *argv[]) {
   while(1) {
     tcpsock as = tcpaccept(ls, -1);
     printf("New Connection\n");
+
+    tcpsend(as, "What's your name?\r\n", 19, -1);
+    tcpflush(as, -1);
+
+    char inbuf[256];
+    size_t size = tcprecvuntil(as, inbuf, sizeof(inbuf), "\r", 1, -1);
+    inbuf[size-1] = 0;
+    char outbuf[256];
+    int rc = snprintf(outbuf, sizeof(outbuf), "Hello, %s!\r\n", inbuf);
+    tcpsend(as, outbuf, rc, -1);
+    tcpflush(as, -1);
+
+    cleanup:
     tcpclose(as);
   }
 
-  printf("%s", "hello world");
   return 0;
 }
